adc0c0b8b7b98e46f99674d72cc741cbd9518e57
abdelazizBi <abdelazizbiad1@gmail.com>
2022-02-11 22:10:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-mfmcvz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-mfmcvz)
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+export default function App() {
+  return (
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+    </div>
+  );
+}
