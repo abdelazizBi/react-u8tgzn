f0549778239616661cc82795520e429fe2ced00f
abdelazizBi <abdelazizbiad1@gmail.com>
2022-02-11 23:27:55 +0100

    statedfull component integration

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import './style.css';
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
 
 export default function App() {
   return (
     <div>
-      <Welcome name="Sara" />
-      <Welcome name="Cahal" />
-      <Welcome name="Edite" />
+      <Clock />
+      <Clock />
+      <Clock />
     </div>
   );
 }
